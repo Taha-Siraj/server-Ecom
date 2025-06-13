42ff5bd05091f36b8c42356ea2d596f7fc68846f
Taha Siraj <Pikachugaming899@gmail.com>
2025-06-13 07:07:22 -0700

    add ecom server

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import db from "./db.js";
 import bcrypt from 'bcryptjs';
 // import cors from "cors";
 
-const app = express();
 // app.use(cors());
+const app = express();
 app.use(express.json());
 
 
@@ -20,7 +20,6 @@ app.get("/", async (req, res) => {
 })
 
 app.post('/signup',async (req , res) => {
-
     let {firstName , lastName , email , password} = req.body;
     if(!firstName || !lastName || !email || !password){
      res.status(400).send({message: "All field Requried"});
@@ -28,19 +27,22 @@ app.post('/signup',async (req , res) => {
     };
     email = email.toLowerCase();
     let query = "SELECT * FROM users WHERE email = $1"
-    let values = [email]
+    let values = [email];
+    console.log("query", query);
     try {
     let result = await db.query(query , values) 
     if(result.rows?.length){
         res.status(400).send({message: "User alreay Exits With This Email"})
-        return
+        console.log(result.rows?.length)
+        return;
     };
     let addQurey = "INSERT INTO users(first_name, last_name, email, password) VALUES($1, $2, $3, $4)"
-    let addValues = [firstName , lastName , email, password]
-    // let addUsers = await db.query(addQurey, addValues) 
     const salt = bcrypt.genSaltSync(10);
-    console.log(salt)
-    // res.status(200).send({message: "testing", results: addUsers})
+    const hash = bcrypt.hashSync(password, salt);
+    let addValues = [firstName , lastName , email, hash]
+    let addUsers = await db.query(addQurey, addValues) 
+    res.status(200).send({message: "testing", results: addUsers})
+    // console.log(salt, hash)
     } catch (error) {
         res.status(400).send({message: "internel server error"})
         console.log(error)

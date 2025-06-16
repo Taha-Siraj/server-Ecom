ccaafff69fcc9dc65e16e98d64aea36c41f350b4
Taha Siraj <Pikachugaming899@gmail.com>
2025-06-16 08:39:21 -0700

    add ecom server

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 let SECRET = process.env.SECRET_key; 
 
 app.get("/", async (req, res) => {
-    try {
-        let result = await db.query("SELECT * FROM users")
-        res.status(200).send({message: result.rows});
-        console.log(result)
+    try { 
+        res.status(200).send({message: "Login Signup / Api"});
     } catch (error) {
-        console.log(error);
         res.status(500).send({message: "internel server error"})
     }
 })

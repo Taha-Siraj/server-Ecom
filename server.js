d3e2001c76766bacd4ef4f6607c778ee27b5311d
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-09 04:17:18 -0700

    app

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ app.use(cors({
   credentials: true 
 }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  next();
-});
-
 
 app.use(cookieParser());
 app.use(express.json());
